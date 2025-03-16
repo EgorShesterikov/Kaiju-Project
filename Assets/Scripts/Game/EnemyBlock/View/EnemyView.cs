b6cadc87c89f614f17560dc209ee78d0d78a2f33
Vladislav Pavlov <wladseverls@gmail.com>
2025-03-16 11:37:36 +0700

    add pool

--- Assets/Scripts/Game/EnemyBlock/View/EnemyView.cs
+++ Assets/Scripts/Game/EnemyBlock/View/EnemyView.cs
@@ -5,6 +5,9 @@ namespace Game.EnemyBlock.View
 {
 	public class EnemyView : MonoBehaviour
 	{
+		private EnemyData _data;
+		public EnemyData Data => _data;
+		
 		public void Initialize(EnemyData enemyData)
 		{
 
